17ee099e4a31ea2d0156aa21c6b653f9bb419231
Ng Pin Yuan <pinyuan13225@gmail.com>
2022-07-22 01:20:18 +0800

    Add project files.

---
+++ Areas/Identity/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace CharityMS.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the User class
+    public class User : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName="nvarchar(100)")]
+        public string FullName { get; set; }
+
+        [PersonalData]
+        public string Address { get; set; }
+
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CharityMS.Areas.Identity.Data;
+using CharityMS.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CharityMS.Areas.Identity.IdentityHostingStartup))]
+namespace CharityMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CharityMSdbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CharityMSdbContextConnection")));
+
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CharityMSdbContext>();
+            });
+        }
+    }
+}

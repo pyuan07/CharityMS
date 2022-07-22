1c3406575171d98df52fc3e17695a08328f83b7e
kb0914 <kaibinyong0914@gmail.com>
2022-07-22 13:31:17 +0800

    20220722 capital characters

--- Models/Donation.cs
+++ Models/Donation.cs
@@ -8,14 +8,14 @@ namespace CharityMS.Models
 {
     public class Donation
     {
-        public Guid id { get; set; }
+        public Guid Id { get; set; }
 
-        public string receiverName;
+        public string ReceiverName;
 
         public User Staff { get; set; }
 
-        public DateTime date { get; set; }
+        public DateTime Date { get; set; }
 
-        public List<string> donations { get; set; }
+        public List<string> Donations { get; set; }
     }
 }
